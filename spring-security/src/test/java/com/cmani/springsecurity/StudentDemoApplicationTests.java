20eb698c28ffbd2ffa119736da9a4f72871e2f41
cmanix07 <cmani.x07@gmail.com>
2020-09-10 16:58:27 +0900

    Added the spring security project - v02

---
+++ spring-security/src/test/java/com/cmani/springsecurity/StudentDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmani.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
